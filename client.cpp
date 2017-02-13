80d7aac005e5bc9082a6f47fd142ad5505390d2b
Reisor <fraserm1989@gmail.com>
2017-02-13 15:03:17 +0100

    Add client and server cpp files.

---
+++ client.cpp
@@ -0,0 +1,70 @@
+#include <enet/enet.h>
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+
+int main(int argc, char *argv[]) {
+    // Initialize ENet.
+    if (enet_initialize() != 0) {
+        fprintf(stderr, "An error occurred while initializing ENet.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    ENetHost *pClient;
+    pClient = enet_host_create (NULL /* create a client host */,
+        1 /* only allow 1 outgoing connection */,
+        2 /* allow up 2 channels to be used, 0 and 1 */,
+        57600 / 8 /* 56K modem with 56 Kbps downstream bandwidth */,
+        14400 / 8 /* 56K modem with 14 Kbps upstream bandwidth */);
+    if (pClient == NULL) {
+        fprintf (stderr, 
+                 "An error occurred while trying to create an ENet client host.\n");
+        exit (EXIT_FAILURE);
+    }
+    
+    ENetAddress address;
+    enet_address_set_host(&address, "localhost");
+    address.port = 27551;
+    ENetPeer *peer = enet_host_connect(pClient, &address, 2, 0);
+    
+    /* Create a reliable packet of size 7 containing "packet\0" */
+    ENetPacket *packet = enet_packet_create("packet", 
+        strlen("packet") + 1, ENET_PACKET_FLAG_RELIABLE);
+    /* Extend the packet so and append the string "foo", so it now */
+    /* contains "packetfoo\0"                                      */
+    //enet_packet_resize(packet, strlen("packetfoo") + 1);
+    //std::strcpy(&packet->data[std::strlen("packet")], "foo");
+    /* Send the packet to the peer over channel id 0. */
+    /* One could also broadcast the packet by         */
+    /* enet_host_broadcast (host, 0, packet);         */
+    enet_peer_send(peer, 0, packet);
+
+    ENetEvent event;
+    enet_peer_disconnect (peer, 0);
+    /* Allow up to 3 seconds for the disconnect to succeed
+     * and drop any packets received packets.
+     */
+    while (enet_host_service (pClient, & event, 3000) > 0)
+    {
+        switch (event.type)
+        {
+        case ENET_EVENT_TYPE_RECEIVE:
+            enet_packet_destroy (event.packet);
+            break;
+        case ENET_EVENT_TYPE_DISCONNECT:
+            puts ("Disconnection succeeded.");
+            break;
+        }
+    }
+    /* We've arrived here, so the disconnect attempt didn't */
+    /* succeed yet.  Force the connection down.             */
+    enet_peer_reset (peer);
+
+    // Close the client.
+    enet_host_destroy(pClient);    
+    // Deinitialize ENet.
+    atexit(enet_deinitialize);
+    return 0;
+}
+
